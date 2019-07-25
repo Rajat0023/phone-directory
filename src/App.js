24e50a7c53a09d1211d0c58ce7a4fcb74487aecc
Rajat0023 <rajatsingh2237@gmail.com>
2019-07-25 19:50:38 +0530

    Created Skeleton of Home Page-final

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* <div>
-      Phone Directory
+        <div className="header">
+          Phone Directory
     </div>
-    <button>Add</button>
-    <div>
-      <span> Name </span><br />
-      <span> Phone </span>
-        </div> */}
-
-        <label htmlFor="name">Name: </label>
-        <input id="name" type="text" placeholder="Type Here" defaultValue="Rajat"></input>
+        <button>Add</button>
+        <div>
+          <span> Name </span><br />
+          <span> Phone </span>
+        </div>
       </div>
 
     );
